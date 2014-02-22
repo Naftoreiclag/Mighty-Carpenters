896bcfcdaf5e565a56bf948983a9f3c113268b33
Naftoreiclag <naftoreiclag@gmail.com>
2014-02-22 12:08:45 -0800

    Copper Ore Texture

--- java/naftoreiclag/zingpower/BlockOreCopper.java
+++ java/naftoreiclag/zingpower/BlockOreCopper.java
@@ -1,8 +1,11 @@
 package naftoreiclag.zingpower;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import naftoreiclag.zingpower.util.MyStaticStrings;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class BlockOreCopper extends Block
@@ -13,4 +16,11 @@ public class BlockOreCopper extends Block
 		this.setBlockName(MyStaticStrings.COPPER_ORE_UNLOCALIZED);
 		this.setCreativeTab(CreativeTabs.tabBlock);
 	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+    public void registerBlockIcons(IIconRegister p_149651_1_)
+    {
+        this.blockIcon = p_149651_1_.registerIcon(MyStaticStrings.MOD_ID + ":copper_ore");
+    }
 }
