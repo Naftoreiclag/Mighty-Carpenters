9c801b4ba7ba2c1298c298c35489d40c5a8a8b7a
Naftoreiclag <naftoreiclag@gmail.com>
2014-02-21 23:14:38 -0800

    Add ZingpowerMod.java

---
+++ java/naftoreiclag/zingpower/ZingpowerMod.java
@@ -0,0 +1,17 @@
+package naftoreiclag.zingpower;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ZingpowerMod.MOD_ID, version = ZingpowerMod.MOD_VER)
+public class ZingpowerMod
+{
+    public static final String MOD_ID = "zingpower";
+    public static final String MOD_VER = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
