eebeb89b5385efb1309ef83de660e2eb420e7a7e
Naftoreiclag <naftoreiclag@gmail.com>
2014-02-22 12:47:32 -0800

    Add static strings for new metals

--- java/naftoreiclag/zingpower/util/MyStaticStrings.java
+++ java/naftoreiclag/zingpower/util/MyStaticStrings.java
@@ -8,6 +8,34 @@ public class MyStaticStrings
 	public static final String UNLOCALIZATION_PREFIX = MOD_ID + ".";
 	
 	public static final String UNLOCALIZED_COPPER_ORE = UNLOCALIZATION_PREFIX + "copperOre";
+	public static final String UNLOCALIZED_LEAD_ORE = UNLOCALIZATION_PREFIX + "leadOre";
+	public static final String UNLOCALIZED_NICKEL_ORE = UNLOCALIZATION_PREFIX + "nickelOre";
+	public static final String UNLOCALIZED_OSMIUM_ORE = UNLOCALIZATION_PREFIX + "osmiumOre";
+	public static final String UNLOCALIZED_SILVER_ORE = UNLOCALIZATION_PREFIX + "silverOre";
+	public static final String UNLOCALIZED_TIN_ORE = UNLOCALIZATION_PREFIX + "tinOre";
+	public static final String UNLOCALIZED_TUNGSTEN_ORE = UNLOCALIZATION_PREFIX + "tungstenOre";
+
+	public static final String UNLOCALIZED_COPPER_INGOT = UNLOCALIZATION_PREFIX + "copperIngot";
+	public static final String UNLOCALIZED_LEAD_INGOT = UNLOCALIZATION_PREFIX + "leadIngot";
+	public static final String UNLOCALIZED_NICKEL_INGOT = UNLOCALIZATION_PREFIX + "nickelIngot";
+	public static final String UNLOCALIZED_OSMIUM_INGOT = UNLOCALIZATION_PREFIX + "osmiumIngot";
+	public static final String UNLOCALIZED_SILVER_INGOT = UNLOCALIZATION_PREFIX + "silverIngot";
+	public static final String UNLOCALIZED_TIN_INGOT = UNLOCALIZATION_PREFIX + "tinIngot";
+	public static final String UNLOCALIZED_TUNGSTEN_INGOT = UNLOCALIZATION_PREFIX + "tungstenIngot";
 	
 	public static final String TEXTURE_COPPER_ORE = MyStaticStrings.MOD_ID + ":copper_ore";
+	public static final String TEXTURE_LEAD_ORE = MyStaticStrings.MOD_ID + ":silver_ore";
+	public static final String TEXTURE_NICKEL_ORE = MyStaticStrings.MOD_ID + ":nickel_ore";
+	public static final String TEXTURE_OSMIUM_ORE = MyStaticStrings.MOD_ID + ":osmium_ore";
+	public static final String TEXTURE_SILVER_ORE = MyStaticStrings.MOD_ID + ":silver_ore";
+	public static final String TEXTURE_TIN_ORE = MyStaticStrings.MOD_ID + ":tin_ore";
+	public static final String TEXTURE_TUNGSTEN_ORE = MyStaticStrings.MOD_ID + ":tungsten_ore";
+	
+	public static final String TEXTURE_COPPER_INGOT = MyStaticStrings.MOD_ID + ":copper_ingot";
+	public static final String TEXTURE_LEAD_INGOT = MyStaticStrings.MOD_ID + ":lead_ingot";
+	public static final String TEXTURE_NICKEL_INGOT = MyStaticStrings.MOD_ID + ":nickel_ingot";
+	public static final String TEXTURE_OSMIUM_INGOT = MyStaticStrings.MOD_ID + ":osmium_ingot";
+	public static final String TEXTURE_SILVER_INGOT = MyStaticStrings.MOD_ID + ":silver_ingot";
+	public static final String TEXTURE_TIN_INGOT = MyStaticStrings.MOD_ID + ":tin_ingot";
+	public static final String TEXTURE_TUNGSTEN_INGOT = MyStaticStrings.MOD_ID + ":tungsten_ingot";
 }
