b53959b763464048bf34d176af60f5aba116ffe6
Naftoreiclag <naftoreiclag@gmail.com>
2014-02-22 17:03:42 -0800

    Add localization for SHF

--- java/naftoreiclag/zingpower/util/MyStaticStrings.java
+++ java/naftoreiclag/zingpower/util/MyStaticStrings.java
@@ -7,6 +7,8 @@ public class MyStaticStrings
 	
 	public static final String UNLOCALIZATION_PREFIX = MOD_ID + ".";
 	
+	// Ores
+	
 	public static final String UNLOCALIZED_COPPER_ORE = UNLOCALIZATION_PREFIX + "copperOre";
 	public static final String UNLOCALIZED_LEAD_ORE = UNLOCALIZATION_PREFIX + "leadOre";
 	public static final String UNLOCALIZED_NICKEL_ORE = UNLOCALIZATION_PREFIX + "nickelOre";
@@ -14,7 +16,16 @@ public class MyStaticStrings
 	public static final String UNLOCALIZED_SILVER_ORE = UNLOCALIZATION_PREFIX + "silverOre";
 	public static final String UNLOCALIZED_TIN_ORE = UNLOCALIZATION_PREFIX + "tinOre";
 	public static final String UNLOCALIZED_TUNGSTEN_ORE = UNLOCALIZATION_PREFIX + "tungstenOre";
+	public static final String TEXTURE_COPPER_ORE = MOD_ID + ":copper_ore";
+	public static final String TEXTURE_LEAD_ORE = MOD_ID + ":silver_ore";
+	public static final String TEXTURE_NICKEL_ORE = MOD_ID + ":nickel_ore";
+	public static final String TEXTURE_OSMIUM_ORE = MOD_ID + ":osmium_ore";
+	public static final String TEXTURE_SILVER_ORE = MOD_ID + ":silver_ore";
+	public static final String TEXTURE_TIN_ORE = MOD_ID + ":tin_ore";
+	public static final String TEXTURE_TUNGSTEN_ORE = MOD_ID + ":tungsten_ore";
 
+	// Ingots
+	
 	public static final String UNLOCALIZED_COPPER_INGOT = UNLOCALIZATION_PREFIX + "copperIngot";
 	public static final String UNLOCALIZED_LEAD_INGOT = UNLOCALIZATION_PREFIX + "leadIngot";
 	public static final String UNLOCALIZED_NICKEL_INGOT = UNLOCALIZATION_PREFIX + "nickelIngot";
@@ -22,20 +33,21 @@ public class MyStaticStrings
 	public static final String UNLOCALIZED_SILVER_INGOT = UNLOCALIZATION_PREFIX + "silverIngot";
 	public static final String UNLOCALIZED_TIN_INGOT = UNLOCALIZATION_PREFIX + "tinIngot";
 	public static final String UNLOCALIZED_TUNGSTEN_INGOT = UNLOCALIZATION_PREFIX + "tungstenIngot";
+	public static final String TEXTURE_COPPER_INGOT = MOD_ID + ":copper_ingot";
+	public static final String TEXTURE_LEAD_INGOT = MOD_ID + ":lead_ingot";
+	public static final String TEXTURE_NICKEL_INGOT = MOD_ID + ":nickel_ingot";
+	public static final String TEXTURE_OSMIUM_INGOT = MOD_ID + ":osmium_ingot";
+	public static final String TEXTURE_SILVER_INGOT = MOD_ID + ":silver_ingot";
+	public static final String TEXTURE_TIN_INGOT = MOD_ID + ":tin_ingot";
+	public static final String TEXTURE_TUNGSTEN_INGOT = MOD_ID + ":tungsten_ingot";
 	
-	public static final String TEXTURE_COPPER_ORE = MyStaticStrings.MOD_ID + ":copper_ore";
-	public static final String TEXTURE_LEAD_ORE = MyStaticStrings.MOD_ID + ":silver_ore";
-	public static final String TEXTURE_NICKEL_ORE = MyStaticStrings.MOD_ID + ":nickel_ore";
-	public static final String TEXTURE_OSMIUM_ORE = MyStaticStrings.MOD_ID + ":osmium_ore";
-	public static final String TEXTURE_SILVER_ORE = MyStaticStrings.MOD_ID + ":silver_ore";
-	public static final String TEXTURE_TIN_ORE = MyStaticStrings.MOD_ID + ":tin_ore";
-	public static final String TEXTURE_TUNGSTEN_ORE = MyStaticStrings.MOD_ID + ":tungsten_ore";
+	// Machines
 	
-	public static final String TEXTURE_COPPER_INGOT = MyStaticStrings.MOD_ID + ":copper_ingot";
-	public static final String TEXTURE_LEAD_INGOT = MyStaticStrings.MOD_ID + ":lead_ingot";
-	public static final String TEXTURE_NICKEL_INGOT = MyStaticStrings.MOD_ID + ":nickel_ingot";
-	public static final String TEXTURE_OSMIUM_INGOT = MyStaticStrings.MOD_ID + ":osmium_ingot";
-	public static final String TEXTURE_SILVER_INGOT = MyStaticStrings.MOD_ID + ":silver_ingot";
-	public static final String TEXTURE_TIN_INGOT = MyStaticStrings.MOD_ID + ":tin_ingot";
-	public static final String TEXTURE_TUNGSTEN_INGOT = MyStaticStrings.MOD_ID + ":tungsten_ingot";
+	// Super-hot Furnace
+	public static final String UNLOCALIZED_SHF = UNLOCALIZATION_PREFIX + "shf";
+	public static final String TEXTURE_SHF_TOP = MOD_ID + ":shf_top";
+	public static final String TEXTURE_SHF_BOTTOM = MOD_ID + ":shf_bottom";
+	public static final String TEXTURE_SHF_SIDE = MOD_ID + ":shf_side";
+	public static final String TEXTURE_SHF_FRONT_ON = MOD_ID + ":shf_front_on";
+	public static final String TEXTURE_SHF_FRONT_OFF = MOD_ID + ":shf_front_off";
 }
--- java/naftoreiclag/zingpower/world/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author james
- *
- */
-package naftoreiclag.zingpower.world;
