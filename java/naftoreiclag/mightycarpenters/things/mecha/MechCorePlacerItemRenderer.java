dc948fe87c31ac67cdbbc0bc0bde9a95c42c8508
Naftoreiclag <naftoreiclag@gmail.com>
2014-03-02 19:25:09 -0800

    Also render as entity

--- java/naftoreiclag/mightycarpenters/things/mecha/MechCorePlacerItemRenderer.java
+++ java/naftoreiclag/mightycarpenters/things/mecha/MechCorePlacerItemRenderer.java
@@ -37,11 +37,27 @@ public class MechCorePlacerItemRenderer implements IItemRenderer
 	{
 		switch(type)
 		{
+			case ENTITY:
+			{
+				GL11.glPushMatrix();
+					GL11.glDisable(GL11.GL_LIGHTING);
+					GL11.glPushMatrix();
+						GL11.glTranslatef((float) 0.5f, (float) 1.5F, (float) 0.5f);
+						Minecraft.getMinecraft().renderEngine.bindTexture(modelTexture);
+						GL11.glPushMatrix();
+							GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
+							this.model.render((Entity) null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
+						GL11.glPopMatrix();
+					GL11.glPopMatrix();
+					GL11.glEnable(GL11.GL_LIGHTING);
+				GL11.glPopMatrix();
+				return;
+			}
+			
 			case EQUIPPED:
 			{
 				GL11.glPushMatrix();
 					GL11.glDisable(GL11.GL_LIGHTING);
-					GL11.glRotatef(180.0f, 0.0f, 1.0f, 0.0f);
 					GL11.glPushMatrix();
 						GL11.glTranslatef((float) 0.5f, (float) 1.5F, (float) 0.5f);
 						Minecraft.getMinecraft().renderEngine.bindTexture(modelTexture);
