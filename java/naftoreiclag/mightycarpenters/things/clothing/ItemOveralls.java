7fceba97eac22694a439be083eb38f78e9efd7b0
Naftoreiclag <naftoreiclag@gmail.com>
2014-03-08 12:40:19 -0800

    Overall, they are overalls
    Why am I wasting so much time on this?

--- java/naftoreiclag/mightycarpenters/things/clothing/ItemOveralls.java
+++ java/naftoreiclag/mightycarpenters/things/clothing/ItemOveralls.java
@@ -1,7 +1,10 @@
 package naftoreiclag.mightycarpenters.things.clothing;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import naftoreiclag.mightycarpenters.MightyCarpentersMod;
 import naftoreiclag.mightycarpenters.util.MyStaticStrings;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemArmor;
@@ -13,8 +16,15 @@ public class ItemOveralls extends ItemArmor
 	public ItemOveralls()
 	{
 		super(MightyCarpentersMod.armor_useless_iron, 1, 2);
+		this.setUnlocalizedName(MyStaticStrings.UNLOCALIZED_OVERALLS);
 		
 		this.setCreativeTab(CreativeTabs.tabFood);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister par1IconRegister)
+	{
+		this.itemIcon = par1IconRegister.registerIcon(MyStaticStrings.TEXTURE_OVERALLS);
 	}
 
 	@Override
--- java/naftoreiclag/mightycarpenters/util/MyStaticStrings.java
+++ java/naftoreiclag/mightycarpenters/util/MyStaticStrings.java
@@ -6,6 +6,7 @@ public class MyStaticStrings
 	public static final String MOD_VER = "0.1.0";
 	
 	public static final String UNLOCALIZATION_PREFIX = MOD_ID + ".";
+	public static final String TEXTURE_PREFIX = MOD_ID + ":";
 	public static final String TILEENTITYID_PREFIX = "tileEntity." + MOD_ID + ".";
 	
 	// Debug
@@ -17,13 +18,13 @@ public class MyStaticStrings
 	public static final String UNLOCALIZED_DEBUG_4 = UNLOCALIZATION_PREFIX + "debug_4";
 	public static final String UNLOCALIZED_DEBUG_5 = UNLOCALIZATION_PREFIX + "debug_5";
 	public static final String UNLOCALIZED_DEBUG_6 = UNLOCALIZATION_PREFIX + "debug_6";
-	public static final String TEXTURE_DEBUG_0 = MOD_ID + ":debug_0";
-	public static final String TEXTURE_DEBUG_1 = MOD_ID + ":debug_1";
-	public static final String TEXTURE_DEBUG_2 = MOD_ID + ":debug_2";
-	public static final String TEXTURE_DEBUG_3 = MOD_ID + ":debug_3";
-	public static final String TEXTURE_DEBUG_4 = MOD_ID + ":debug_4";
-	public static final String TEXTURE_DEBUG_5 = MOD_ID + ":debug_5";
-	public static final String TEXTURE_DEBUG_6 = MOD_ID + ":debug_6";
+	public static final String TEXTURE_DEBUG_0 = TEXTURE_PREFIX + "debug_0";
+	public static final String TEXTURE_DEBUG_1 = TEXTURE_PREFIX + "debug_1";
+	public static final String TEXTURE_DEBUG_2 = TEXTURE_PREFIX + "debug_2";
+	public static final String TEXTURE_DEBUG_3 = TEXTURE_PREFIX + "debug_3";
+	public static final String TEXTURE_DEBUG_4 = TEXTURE_PREFIX + "debug_4";
+	public static final String TEXTURE_DEBUG_5 = TEXTURE_PREFIX + "debug_5";
+	public static final String TEXTURE_DEBUG_6 = TEXTURE_PREFIX + "debug_6";
 	
 	// Metals
 	// ===
@@ -36,13 +37,13 @@ public class MyStaticStrings
 	public static final String UNLOCALIZED_SILVER_ORE = UNLOCALIZATION_PREFIX + "silver_ore";
 	public static final String UNLOCALIZED_TIN_ORE = UNLOCALIZATION_PREFIX + "tin_ore";
 	public static final String UNLOCALIZED_TUNGSTEN_ORE = UNLOCALIZATION_PREFIX + "tungsten_ore";
-	public static final String TEXTURE_COPPER_ORE = MOD_ID + ":copper_ore";
-	public static final String TEXTURE_LEAD_ORE = MOD_ID + ":silver_ore";
-	public static final String TEXTURE_NICKEL_ORE = MOD_ID + ":nickel_ore";
-	public static final String TEXTURE_OSMIUM_ORE = MOD_ID + ":osmium_ore";
-	public static final String TEXTURE_SILVER_ORE = MOD_ID + ":silver_ore";
-	public static final String TEXTURE_TIN_ORE = MOD_ID + ":tin_ore";
-	public static final String TEXTURE_TUNGSTEN_ORE = MOD_ID + ":tungsten_ore";
+	public static final String TEXTURE_COPPER_ORE = TEXTURE_PREFIX + "copper_ore";
+	public static final String TEXTURE_LEAD_ORE = TEXTURE_PREFIX + "silver_ore";
+	public static final String TEXTURE_NICKEL_ORE = TEXTURE_PREFIX + "nickel_ore";
+	public static final String TEXTURE_OSMIUM_ORE = TEXTURE_PREFIX + "osmium_ore";
+	public static final String TEXTURE_SILVER_ORE = TEXTURE_PREFIX + "silver_ore";
+	public static final String TEXTURE_TIN_ORE = TEXTURE_PREFIX + "tin_ore";
+	public static final String TEXTURE_TUNGSTEN_ORE = TEXTURE_PREFIX + "tungsten_ore";
 
 	// Ingots
 	public static final String UNLOCALIZED_COPPER_INGOT = UNLOCALIZATION_PREFIX + "copper_ingot";
@@ -52,71 +53,71 @@ public class MyStaticStrings
 	public static final String UNLOCALIZED_SILVER_INGOT = UNLOCALIZATION_PREFIX + "silver_ingot";
 	public static final String UNLOCALIZED_TIN_INGOT = UNLOCALIZATION_PREFIX + "tin_ingot";
 	public static final String UNLOCALIZED_TUNGSTEN_INGOT = UNLOCALIZATION_PREFIX + "tungsten_ingot";
-	public static final String TEXTURE_COPPER_INGOT = MOD_ID + ":copper_ingot";
-	public static final String TEXTURE_LEAD_INGOT = MOD_ID + ":lead_ingot";
-	public static final String TEXTURE_NICKEL_INGOT = MOD_ID + ":nickel_ingot";
-	public static final String TEXTURE_OSMIUM_INGOT = MOD_ID + ":osmium_ingot";
-	public static final String TEXTURE_SILVER_INGOT = MOD_ID + ":silver_ingot";
-	public static final String TEXTURE_TIN_INGOT = MOD_ID + ":tin_ingot";
-	public static final String TEXTURE_TUNGSTEN_INGOT = MOD_ID + ":tungsten_ingot";
+	public static final String TEXTURE_COPPER_INGOT = TEXTURE_PREFIX + "copper_ingot";
+	public static final String TEXTURE_LEAD_INGOT = TEXTURE_PREFIX + "lead_ingot";
+	public static final String TEXTURE_NICKEL_INGOT = TEXTURE_PREFIX + "nickel_ingot";
+	public static final String TEXTURE_OSMIUM_INGOT = TEXTURE_PREFIX + "osmium_ingot";
+	public static final String TEXTURE_SILVER_INGOT = TEXTURE_PREFIX + "silver_ingot";
+	public static final String TEXTURE_TIN_INGOT = TEXTURE_PREFIX + "tin_ingot";
+	public static final String TEXTURE_TUNGSTEN_INGOT = TEXTURE_PREFIX + "tungsten_ingot";
 	
 	// Engineering Hand Tools
 	// ===
 	
 	// Blowtorch
 	public static final String UNLOCALIZED_BLOWTORCH = UNLOCALIZATION_PREFIX + "blowtorch";
-	public static final String TEXTURE_BLOWTORCH = MOD_ID + ":blowtorch_32";
+	public static final String TEXTURE_BLOWTORCH = TEXTURE_PREFIX + "blowtorch_32";
 	
 	// Chainsaw
 	public static final String UNLOCALIZED_CHAINSAW = UNLOCALIZATION_PREFIX + "chainsaw";
-	public static final String TEXTURE_CHAINSAW = MOD_ID + ":chainsaw_32";
+	public static final String TEXTURE_CHAINSAW = TEXTURE_PREFIX + "chainsaw_32";
 	
 	// Drill
 	public static final String UNLOCALIZED_DRILL = UNLOCALIZATION_PREFIX + "drill";
-	public static final String TEXTURE_DRILL = MOD_ID + ":drill_32";
+	public static final String TEXTURE_DRILL = TEXTURE_PREFIX + "drill_32";
 	
 	// Glue
 	public static final String UNLOCALIZED_GLUE = UNLOCALIZATION_PREFIX + "glue";
-	public static final String TEXTURE_GLUE = MOD_ID + ":glue_32";
+	public static final String TEXTURE_GLUE = TEXTURE_PREFIX + "glue_32";
 	
 	// Hammer
 	public static final String UNLOCALIZED_HAMMER = UNLOCALIZATION_PREFIX + "hammer";
-	public static final String TEXTURE_HAMMER = MOD_ID + ":hammer_32";
+	public static final String TEXTURE_HAMMER = TEXTURE_PREFIX + "hammer_32";
 	
 	// Mallet
 	public static final String UNLOCALIZED_MALLET = UNLOCALIZATION_PREFIX + "mallet";
-	public static final String TEXTURE_MALLET = MOD_ID + ":mallet_32";
+	public static final String TEXTURE_MALLET = TEXTURE_PREFIX + "mallet_32";
 	
 	// Nailgun
 	public static final String UNLOCALIZED_NAILGUN = UNLOCALIZATION_PREFIX + "nailgun";
-	public static final String TEXTURE_NAILGUN = MOD_ID + ":nailgun_32";
-	public static final String SOUND_NAILGUN_FIRE = MOD_ID + ":nailgun_fire";
+	public static final String TEXTURE_NAILGUN = TEXTURE_PREFIX + "nailgun_32";
+	public static final String SOUND_NAILGUN_FIRE = TEXTURE_PREFIX + "nailgun_fire";
 	
 	// Saw
 	public static final String UNLOCALIZED_SAW = UNLOCALIZATION_PREFIX + "saw";
-	public static final String TEXTURE_SAW = MOD_ID + ":saw_32";
+	public static final String TEXTURE_SAW = TEXTURE_PREFIX + "saw_32";
 	
 	// Scissors
 	public static final String UNLOCALIZED_SCISSORS = UNLOCALIZATION_PREFIX + "scissors";
-	public static final String TEXTURE_SCISSORS = MOD_ID + ":scissors_32";
+	public static final String TEXTURE_SCISSORS = TEXTURE_PREFIX + "scissors_32";
 	
 	// Screwdriver
 	public static final String UNLOCALIZED_SCREWDRIVER = UNLOCALIZATION_PREFIX + "screwdriver";
-	public static final String TEXTURE_SCREWDRIVER = MOD_ID + ":screwdriver_32";
+	public static final String TEXTURE_SCREWDRIVER = TEXTURE_PREFIX + "screwdriver_32";
 	
 	// Tape
 	public static final String UNLOCALIZED_TAPE = UNLOCALIZATION_PREFIX + "tape";
-	public static final String TEXTURE_TAPE = MOD_ID + ":tape_32";
+	public static final String TEXTURE_TAPE = TEXTURE_PREFIX + "tape_32";
 	
 	// Monkey Wrench
 	public static final String UNLOCALIZED_WRENCH = UNLOCALIZATION_PREFIX + "wrench";
-	public static final String TEXTURE_WRENCH = MOD_ID + ":wrench_32";
+	public static final String TEXTURE_WRENCH = TEXTURE_PREFIX + "wrench_32";
 	
 	
 	// Mech
 	// ====
 	public static final String UNLOCALIZED_MECH_CORE_PLACER = UNLOCALIZATION_PREFIX + "mech_core_placer";
-	public static final String MODEL_TEXTURE_METAL_MECHA_CORE = MOD_ID + ":textures/models/metal_mech_core.png";
+	public static final String MODEL_TEXTURE_METAL_MECHA_CORE = TEXTURE_PREFIX + "textures/models/metal_mech_core.png";
 	
 	public static final String ENTITY_ID_STRING_MECH_ROOT = "MechRoot";
 	public static final String ENTITY_ID_STRING_MECH_PART = "MechPart";
@@ -124,59 +125,61 @@ public class MyStaticStrings
 	public static final int ENTITY_ID_INT_MECH_PART = 70;
 	
 
-	public static final String SOUND_METAL_HIT_PREFIX = MOD_ID + ":metal_hit_";
+	public static final String SOUND_METAL_HIT_PREFIX = TEXTURE_PREFIX + "metal_hit_";
 	public static final String SOUND_METAL_HIT_1 = SOUND_METAL_HIT_PREFIX + "1";
 	public static final String SOUND_METAL_HIT_2 = SOUND_METAL_HIT_PREFIX + "2";
 	
 	//
-	public static final String MODEL_TEXTURE_OVERALLS = MOD_ID + ":textures/models/overalls.png";
+	public static final String UNLOCALIZED_OVERALLS = UNLOCALIZATION_PREFIX + "overalls";
+	public static final String TEXTURE_OVERALLS = TEXTURE_PREFIX + "overalls";
+	public static final String MODEL_TEXTURE_OVERALLS = TEXTURE_PREFIX + "textures/models/armor/overalls.png";
 	
 	// ???
 	// ===
 	
 	// Steel Scaffold
 	public static final String UNLOCALIZED_SCAFFOLD = UNLOCALIZATION_PREFIX + "steel_scaffold";
-	public static final String TEXTURE_SCAFFOLD = MOD_ID + ":scaffold_iron";
+	public static final String TEXTURE_SCAFFOLD = TEXTURE_PREFIX + "scaffold_iron";
 	public static final String UNLOCALIZED_SCAFFOLD_FENCE = UNLOCALIZATION_PREFIX + "steel_scaffoldFence";
-	public static final String TEXTURE_SCAFFOLD_FENCE = MOD_ID + ":scaffold_iron_fence";
+	public static final String TEXTURE_SCAFFOLD_FENCE = TEXTURE_PREFIX + "scaffold_iron_fence";
 	
 	// Concrete
 	public static final String UNLOCALIZED_CONCRETE = UNLOCALIZATION_PREFIX + "concrete";
-	public static final String TEXTURE_CONCRETE = MOD_ID + ":concrete";
+	public static final String TEXTURE_CONCRETE = TEXTURE_PREFIX + "concrete";
 	
 	// Blueprint
 	public static final String UNLOCALIZED_BLUEPRINT = UNLOCALIZATION_PREFIX + "blueprint";
-	public static final String TEXTURE_BLUEPRINT_BLANK = MOD_ID + ":blueprint_blank";
-	public static final String TEXTURE_BLUEPRINT_WRITTEN = MOD_ID + ":blueprint_written";
+	public static final String TEXTURE_BLUEPRINT_BLANK = TEXTURE_PREFIX + "blueprint_blank";
+	public static final String TEXTURE_BLUEPRINT_WRITTEN = TEXTURE_PREFIX + "blueprint_written";
 	
 	// Sketch Station
 	public static final String UNLOCALIZED_SKETCH_STATION = UNLOCALIZATION_PREFIX + "sketchStation";
-	public static final String TEXTURE_SKETCH_STATION_FRONT_LEFT = MOD_ID + ":sketch_station_front_left";
-	public static final String TEXTURE_SKETCH_STATION_FRONT_RIGHT = MOD_ID + ":sketch_station_front_right";
-	public static final String TEXTURE_SKETCH_STATION_BACK_LEFT = MOD_ID + ":sketch_station_back_left";
-	public static final String TEXTURE_SKETCH_STATION_BACK_RIGHT = MOD_ID + ":sketch_station_back_right";
-	public static final String TEXTURE_SKETCH_STATION_SIDE = MOD_ID + ":sketch_station_side";
-	public static final String TEXTURE_SKETCH_STATION_DEFAULT = MOD_ID + ":sketch_station_default";
-	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_LEFT_0 = MOD_ID + ":sketch_station_top_empty_left_0";
-	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_LEFT_1 = MOD_ID + ":sketch_station_top_empty_left_1";
-	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_LEFT_2 = MOD_ID + ":sketch_station_top_empty_left_2";
-	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_LEFT_3 = MOD_ID + ":sketch_station_top_empty_left_3";
-	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_RIGHT_0 = MOD_ID + ":sketch_station_top_empty_right_0";
-	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_RIGHT_1 = MOD_ID + ":sketch_station_top_empty_right_1";
-	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_RIGHT_2 = MOD_ID + ":sketch_station_top_empty_right_2";
-	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_RIGHT_3 = MOD_ID + ":sketch_station_top_empty_right_3";
-	public static final String TEXTURE_SKETCH_STATION_TOP_FILLED_LEFT = MOD_ID + ":sketch_station_top_filled_left";
-	public static final String TEXTURE_SKETCH_STATION_TOP_FILLED_RIGHT = MOD_ID + ":sketch_station_top_filled_right";
+	public static final String TEXTURE_SKETCH_STATION_FRONT_LEFT = TEXTURE_PREFIX + "sketch_station_front_left";
+	public static final String TEXTURE_SKETCH_STATION_FRONT_RIGHT = TEXTURE_PREFIX + "sketch_station_front_right";
+	public static final String TEXTURE_SKETCH_STATION_BACK_LEFT = TEXTURE_PREFIX + "sketch_station_back_left";
+	public static final String TEXTURE_SKETCH_STATION_BACK_RIGHT = TEXTURE_PREFIX + "sketch_station_back_right";
+	public static final String TEXTURE_SKETCH_STATION_SIDE = TEXTURE_PREFIX + "sketch_station_side";
+	public static final String TEXTURE_SKETCH_STATION_DEFAULT = TEXTURE_PREFIX + "sketch_station_default";
+	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_LEFT_0 = TEXTURE_PREFIX + "sketch_station_top_empty_left_0";
+	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_LEFT_1 = TEXTURE_PREFIX + "sketch_station_top_empty_left_1";
+	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_LEFT_2 = TEXTURE_PREFIX + "sketch_station_top_empty_left_2";
+	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_LEFT_3 = TEXTURE_PREFIX + "sketch_station_top_empty_left_3";
+	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_RIGHT_0 = TEXTURE_PREFIX + "sketch_station_top_empty_right_0";
+	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_RIGHT_1 = TEXTURE_PREFIX + "sketch_station_top_empty_right_1";
+	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_RIGHT_2 = TEXTURE_PREFIX + "sketch_station_top_empty_right_2";
+	public static final String TEXTURE_SKETCH_STATION_TOP_EMPTY_RIGHT_3 = TEXTURE_PREFIX + "sketch_station_top_empty_right_3";
+	public static final String TEXTURE_SKETCH_STATION_TOP_FILLED_LEFT = TEXTURE_PREFIX + "sketch_station_top_filled_left";
+	public static final String TEXTURE_SKETCH_STATION_TOP_FILLED_RIGHT = TEXTURE_PREFIX + "sketch_station_top_filled_right";
 	public static final String TEID_SKETCH_STATION = TILEENTITYID_PREFIX + "sketch_station";
 	
 	// Super-hot Furnace
 	public static final String TEID_SHF = TILEENTITYID_PREFIX + "shf";
 	public static final String UNLOCALIZED_SHF = UNLOCALIZATION_PREFIX + "shf";
-	public static final String TEXTURE_SHF_TOP = MOD_ID + ":shf_top";
-	public static final String TEXTURE_SHF_BOTTOM = MOD_ID + ":shf_bottom";
-	public static final String TEXTURE_SHF_SIDE = MOD_ID + ":shf_side";
-	public static final String TEXTURE_SHF_FRONT_ON = MOD_ID + ":shf_front_on";
-	public static final String TEXTURE_SHF_FRONT_OFF = MOD_ID + ":shf_front_off";
+	public static final String TEXTURE_SHF_TOP = TEXTURE_PREFIX + "shf_top";
+	public static final String TEXTURE_SHF_BOTTOM = TEXTURE_PREFIX + "shf_bottom";
+	public static final String TEXTURE_SHF_SIDE = TEXTURE_PREFIX + "shf_side";
+	public static final String TEXTURE_SHF_FRONT_ON = TEXTURE_PREFIX + "shf_front_on";
+	public static final String TEXTURE_SHF_FRONT_OFF = TEXTURE_PREFIX + "shf_front_off";
 	public static final int GUIID_SHF = 0;
 	
 	// Burning Generator
