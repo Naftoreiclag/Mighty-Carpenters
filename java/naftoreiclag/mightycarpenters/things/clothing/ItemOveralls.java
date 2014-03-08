7a2f476bef18ad22af0c6d1d012eb5d014cadde4
Naftoreiclag <naftoreiclag@gmail.com>
2014-03-08 08:55:00 -0800

    Overalls

---
+++ java/naftoreiclag/mightycarpenters/things/clothing/ItemOveralls.java
@@ -0,0 +1,15 @@
+package naftoreiclag.mightycarpenters.things.clothing;
+
+import naftoreiclag.mightycarpenters.MightyCarpentersMod;
+import net.minecraft.item.ItemArmor;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class ItemOveralls extends ItemArmor
+{
+
+	public ItemOveralls()
+	{
+		super(MightyCarpentersMod.armor_useless_iron, 1, 2);
+	}
+	
+}
